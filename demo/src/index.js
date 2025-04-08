397f76f4a338d89f828033e14982ad3833d0f623
Tika299 <vukofa9120@gmail.com>
2025-04-08 21:54:32 +0700

    commit scss, chia component

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,6 +5,8 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname,'public')))
+
 app.use(morgan('combined'))
 
 app.engine('hbs', engine({
