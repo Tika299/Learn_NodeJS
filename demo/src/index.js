3bfe2ca11f660f741d408e284e6ffd398ae4ae49
Tika299 <vukofa9120@gmail.com>
2026-01-15 10:50:23 +0700

    completed

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const morgan = require('morgan');
-const { engine } = require('express-handlebars');
-const path = require('path');
-const app = express();
-const port = 3000;
+                const express = require('express');
+                const morgan = require('morgan');
+                const { engine } = require('express-handlebars');
+                const path = require('path');
+                const app = express();
+                const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
