339e15d1f5a975d240a1f575204439ef639f10db
Tika299 <vukofa9120@gmail.com>
2026-01-15 10:47:33 +0700

    Intall completed

--- demo/src/index.js
+++ demo/src/index.js
@@ -9,13 +9,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('combined'));
 
-app.engine(
-    'hbs',
-    engine({
-        defaultLayout: 'main',
-        extname: '.hbs',
-    }),
-);
+        app.engine(
+            'hbs',
+            engine({
+                defaultLayout: 'main',
+                extname: '.hbs',
+            }),
+        );
           app.set('view engine', 'hbs');
           app.set('views', path.join(__dirname, 'resources/views'));
 
